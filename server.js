fbce1d42e4b7ed44eaca7244b03a83aa9b4e14da
ChadiSeif <84188405+ChadiSeif@users.noreply.github.com>
2021-10-04 14:25:29 +0100

    Update and rename Server.js to server.js

--- server.js
+++ server.js
@@ -31,6 +31,9 @@ app.listen(port, (error) => {
 ///HEROKU
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 //connect to Db :
